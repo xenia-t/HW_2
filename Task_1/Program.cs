33022393319310b3be2beb1fa96b38ec7a73294b
Xenia <listk98@yandex.ru>
2022-03-02 14:25:55 +0300

    Add Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите трехзначное число: ");
+string numberA = Console.ReadLine();
+int A = int.Parse(numberA);
+
+int second_digit = A/10%10;
+Console.WriteLine($"Вторая цифра числа {A}: {second_digit }");
+
